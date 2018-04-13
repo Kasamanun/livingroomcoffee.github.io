a86c0a27338076bed621ca3ada6bc40b3d6f7685
OpalK <kasamanun@gmail.com>
2018-04-13 02:57:32 -0600

    edit

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -5,14 +5,14 @@
 
 
     $email_from='livingroomcoffee.tk';
-    $email_subject="Feedback"
+    $email_subject="Feedback";
     $email_body="User Name: $name.\n".
                     "User Email: $visitor_email.\n".
                         "User Message: $message.\n";
 
     $to="livingroomcoffee@yahoo.com";
     $headers="From: $email_from \r\n";
-    $headers .="Reply-To: $visitor_email \r\n";
+    $headers="Reply-To: $visitor_email \r\n";
 
 
     mail($to,$email_subject,$email_body,$headers);
